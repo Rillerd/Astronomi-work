228023a5b136ffb1f799df6870a6ffc3ed1ad327
Rillerd <rikard.lesley@gmail.com>
2024-11-21 17:52:43 +0100

    Update Exercise1.py
    Now exercise 1 and 2

--- Exercise1.py
+++ Exercise1.py
@@ -40,7 +40,7 @@ def mean_energy(T, norm_const, Q_0):
 
 #################################################################################################################################################################
 #######                                           The 3 functions below are required for part B                                                           #######
-
+"""
 def AGN_luminosity(energy): return 1e41 * (energy / 13.6) ** (-1.5)
 
 
@@ -54,7 +54,7 @@ def AGN_mean_energy(Q_0):
     integrand = lambda energy: AGN_luminosity(energy)  # the ionizing luminosity
     L_ionizing, _ = quad(integrand, 13.6, np.inf)
     return L_ionizing / Q_0
-
+"""
 #################################################################################################################################################################
 
 
@@ -65,34 +65,119 @@ def stromgren_radius(Q_0): return ((3 * Q_0) / (4 * np.pi * n_H**2 * alpha_B))**
 
 
 def hydrogen_number_density(r, R_SO, sigma):
-    n_H_neutral = (3 * (r/R_SO) ** 2) / ((1 - (r/R_SO) ** 3) * n_H * sigma * R_SO)
-    n_H_plus = 1 - n_H_neutral
-    return n_H_neutral, n_H_plus
+    n_H_neutral_ratio = (3 * (r/R_SO) ** 2) / ((1 - (r/R_SO) ** 3) * n_H * sigma * R_SO)
+    n_H_plus_ratio = 1 - n_H_neutral_ratio
+    return n_H_neutral_ratio, n_H_plus_ratio
 
 
 def optical_depth(radius, n_H_neutral): return n_H_neutral * sigma_0 * radius  # at the ionization threshold for hydrogen, our cross-section is the standard sigma_0
 
 
-def radius_values(R_SO): return np.linspace(0, 1.2 * R_SO, 100000)
+def radius_values(R_SO): return np.linspace(0, 1.2 * R_SO, 1000)
 
+
+####################################################################################################################################################################################
+                                  ############################## Exercise 2   ##############################
+
+def heating(T, density_ion_ratio): 
+    n_H_plus = density_ion_ratio * n_H
+    return n_H_plus * n_H * alpha_B * (3 / 2) * k_B * T # in book says alpha_B but in notes it was alpha, different?
+
+
+def recombination_cooling(T, density_ion_ratio):   
+    n_H_plus = density_ion_ratio * n_H
+    return n_H_plus ** 2 *  alpha_B * (0.684 - 0.0416 * np.log(T/1e4)) * k_B * T
+
+
+def free_free_cooling(T, density_ion_ratio): 
+    n_H_plus = density_ion_ratio * n_H
+    return 0.54 * n_H_plus ** 2 * alpha_B * (T / 1e4) ** 0.37 * k_B * T
+
+
+def collisional_cooling(T, density_ion_ratio, density_neutral_ratio): 
+
+    n_H_plus = density_ion_ratio * n_H
+    n_H_neutral = density_neutral_ratio * n_H
+
+    A_21 = 4.69e8  # s^-1
+    A_2gamma = 8.23  # s^-1  dont know if want the forbidden one also
+    energy_difference = 10.23 * 1.6e-12  # erg
+    omega_12 = 0.69  # for T = 2e4 K
+    omega_gamma2 = 0.35  # for T = 2e4 K 
+    gaunt_factor_S = 2
+    gaunt_factor_P = 6
+
+    q_22P  = (8.629e-6 * omega_12) / (T ** (1/2) * gaunt_factor_P )  * np.exp(-energy_difference / (k_B * T))# collision excitation rate coefficient for 2^2P
+    q_22S  = (8.629e-6 * omega_gamma2) / ( T ** (1/2) * gaunt_factor_S) *  np.exp(-energy_difference / (k_B * T))# collision excitation rate coefficient 2^2S
+
+    return n_H_plus * n_H_neutral * (A_2gamma * q_22S +  A_21 * q_22P) * energy_difference  # sedan plottad över n_H * n_e 
+
+
+def total_cooling(T, density_ion_ratio, density_neutral_ratio): return free_free_cooling(T, density_ion_ratio) + recombination_cooling(T, density_ion_ratio) + collisional_cooling(T, density_ion_ratio, density_neutral_ratio)
+
+
+def plotting_all_rates(density_ion_ratio_values, density_neutral_ratio_values):
+    ff_values = []
+    rr_values = []
+    ce_values = []
+    heating_values = []
+    cooling_values = []
+    temperatures = np.logspace(3,5, 100)
+
+    for density_ion_ratio, density_neutral_ratio in zip(density_ion_ratio_values, density_neutral_ratio_values):
+        ion_density = density_ion_ratio * n_H
+        neutral_density = density_neutral_ratio * n_H
+        norm_factor = ion_density * n_H
+
+        for T in temperatures:
+            ff = free_free_cooling(T, ion_density)
+            rr = recombination_cooling(T, ion_density)
+            ce = collisional_cooling(T, ion_density, neutral_density)
+            hr = heating(T, ion_density)
+            cooling = total_cooling(T, ion_density, neutral_density)
+            ff_values.append(ff / norm_factor)
+            rr_values.append(rr / norm_factor)
+            ce_values.append(ce / norm_factor)
+            heating_values.append(hr / norm_factor)
+            cooling_values.append(cooling / norm_factor)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(temperatures, heating_values, label="Heating", color="orange")
+    plt.plot(temperatures, rr_values, label="Recombination Cooling", color="blue")
+    plt.plot(temperatures, ff_values, label="Free-Free Cooling", color="green")
+    plt.plot(temperatures, ce_values, label="Collisional Cooling", color="red")
+    plt.plot(temperatures, cooling_values, label="Total Cooling", color="purple", linestyle="--")
+
+    # Labels and legend
+    plt.xlabel("Temperature [K]")
+    plt.ylabel("Rate per n_e n_H [erg cm^3 s^-1]")
+    plt.xlim(1000, 7000)
+    plt.ylim(1e-26, 1e-24)
+    plt.yscale("log")
+    plt.title("Normalized Heating and Cooling Rates vs Temperature")
+    plt.tick_params(axis="both", which="both", direction="in", top=True, right=True)
+    plt.legend()
+    plt.show()
+    
+##################################################################################################################################################################################################################
 
 def densities_and_optical_depth(radiuses, R_SO, sigma):
 
-    density_neutral_values = []
-    density_ion_values = []
+    density_neutral_ratio_values = []
+    density_ion_ratio_values = []
     optical_depth_values = []
 
     for radius in radiuses:
-        density_neutral, density_ion = hydrogen_number_density(radius, R_SO, sigma)
-        tau = optical_depth(radius, density_neutral)
-        density_neutral_values.append(density_neutral)
-        density_ion_values.append(density_ion)
+        density_neutral_ratio, density_ion_ratio = hydrogen_number_density(radius, R_SO, sigma)
+        tau = optical_depth(radius, density_neutral_ratio)
+        density_neutral_ratio_values.append(density_neutral_ratio)
+        density_ion_ratio_values.append(density_ion_ratio)
         optical_depth_values.append(tau)
     
     
     pc_radiuses = radiuses / (3.1e18)
     
-    return pc_radiuses, density_neutral_values, density_ion_values, optical_depth_values
+    return pc_radiuses, density_neutral_ratio_values, density_ion_ratio_values, optical_depth_values
 
 
 def plotting(pc_radius, density_neutral_values, density_ion_values, optical_depth_values, title):
@@ -123,22 +208,66 @@ def main():
     R_SO = stromgren_radius(Q_0)
     sigma = cross_section(E_mean)
     radiuses = radius_values(R_SO)
-    pc_radiuses, density_neutral_values, density_ion_values, tau = densities_and_optical_depth(radiuses, R_SO, sigma)
-
-    print(f"\nO3 supernova\nR_SO = {R_SO / 3.1e18:.2f} pc, Q_0 = {Q_0:.1e}, <E> = {E_mean:.1f} eV, cross-section = {sigma:.1e} cm^2")
-    plotting(pc_radiuses, density_neutral_values, density_ion_values, tau, "O3 star")
- 
+    pc_radiuses, density_neutral_ratio_values, density_ion_ratio_values, tau = densities_and_optical_depth(radiuses, R_SO, sigma)
+    plotting_all_rates(density_ion_ratio_values, density_neutral_ratio_values)
+    #print(f"\nO3 supernova\nR_SO = {R_SO / 3.1e18:.2f} pc, Q_0 = {Q_0:.1e}, <E> = {E_mean:.1f} eV, cross-section = {sigma:.1e} cm^2")
+    #plotting(pc_radiuses, density_neutral_ratio_values, density_ion_ratio_values, tau, "O3 star")
+    """
     Q_0 = AGN_Q_0()
     E_mean = AGN_mean_energy(Q_0)
     R_SO = stromgren_radius(Q_0)
     sigma = cross_section(E_mean)
     radiuses = radius_values(R_SO)
-    pc_radiuses, density_neutral_values, density_ion_values, tau = densities_and_optical_depth(radiuses, R_SO, sigma)
+    pc_radiuses, density_neutral_ratio_values, density_ion_ratio_values, tau = densities_and_optical_depth(radiuses, R_SO, sigma)
+
+    
 
     print(f"\nAGN: \nR_SO = {R_SO / 3.1e18:.5f} pc, Q_0 = {Q_0:.1e}, <E> = {E_mean:.1f} eV, cross-section = {sigma:.1e} cm^2")
-    plotting(pc_radiuses, density_neutral_values, density_ion_values, tau, "AGN")
-
+    plotting(pc_radiuses, density_neutral_ratio_values, density_ion_ratio_values, tau, "AGN")
+    """
 
 
 if __name__ == "__main__":
     main()
+
+
+###   Exercise 2 
+
+"""
+def heating(T, density_ion_ratio): 
+    n_H_plus = density_ion_ratio * n_H
+    return n_H_plus * n_H * alpha_B * (3 / 2) * k_B * T # in book says alpha_B but in notes it was alpha, different?
+
+
+def recombination_cooling(T, density_ion_ratio):   
+    n_H_plus = density_ion_ratio * n_H
+    return n_H_plus ** 2 *  alpha_B * (0.684 - 0.0416 * np.log(T/1e4)) * k_B * T
+
+
+def free_free_cooling(T, density_ion_ratio): 
+    n_H_plus = density_ion_ratio * n_H
+    return 0.54 * n_H_plus ** 2 * alpha_B * (T / 1e4) ** 0.37 * k_B * T
+
+
+def collisional_cooling(T, density_ion_ratio, density_neutral_ratio): 
+
+    n_H_plus = density_ion_ratio * n_H
+    n_H_neutral = density_neutral_ratio * n_H
+
+    A_21 = 4.69e8  # s^-1
+    A_2gamma = 8.23  # s^-1  dont know if want the forbidden one also
+    energy_difference = 10.23  # eV
+    omega_12 = 0.69  # for T = 2e4 K
+    omega_gamma2 = 0.35  # for T = 2e4 K 
+    gaunt_factor_S = 2
+    gaunt_factor_P = 6
+
+    q_22P  = (8.629e-6 * omega_12) / (T ** (1/2) * gaunt_factor_P )  * np.exp(-energy_difference / (k_B * T))# collision excitation rate coefficient for 2^2P
+    q_22S  = (8.629e-6 * omega_gamma2) / ( T ** (1/2) * gaunt_factor_S) *  np.exp(-energy_difference / (k_B * T))# collision excitation rate coefficient 2^2S
+
+    return n_H_plus * n_H_neutral * (A_2gamma * q_22S +  A_21 * q_22P) * energy_difference  # sedan plottad över n_H * n_e 
+
+
+def total_cooling(T, n_H_plus): return free_free_cooling(T, n_H_plus) + recombination_cooling(T, n_H_plus) + collisional_cooling(T)
+
+"""
